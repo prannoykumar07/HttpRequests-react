ceef6516ce0efedacbda26845d043f761a2623ce
prannoykumar07 <prannkumar@deloitte.com>
2019-12-28 16:14:10 +0530

    Ch. 196

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            selectedPostId: null
+        }
+    }
+
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId: id });
+    }
+
+    componentDidMount() {
+        Axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return { ...post, author: 'Max' }
+                })
+                this.setState({ posts: updatedPosts });
+                console.log(response);
+            });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => { this.postSelectedHandler(post.id) }} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
